85f33c3ac3940e189f497986b2bf437b6f22efc5
ELFANNIR ABDERRAHMANE <aelfannir@gmail.com>
2024-03-12 10:25:21 +0000

    ACHAT-150

--- src/_custom/DetailView/DetailView.tsx
+++ src/_custom/DetailView/DetailView.tsx
@@ -1,132 +1,27 @@
-import React, {Fragment, HTMLAttributes, useMemo} from 'react';
-import {useMapping} from '../hooks/UseMapping';
-import {ItemView} from '../components/ItemView';
-import {Skeleton} from '@mui/material';
-import {Trans} from '../components/Trans';
-import clsx from 'clsx';
-import {ColumnDef, ColumnMapping, DetailColumns, DetailViewType, Model, ViewEnum} from '../types/ModelMapping';
-import {TitleContent} from '../ListingView/views/Table/HeaderCell';
-import {ListingView} from '../ListingView/ListingView';
-import {Link, useParams} from 'react-router-dom';
-import {DetailViewColumnContent} from './DetailViewColumnContent';
-import {useAuth} from '../hooks/UseAuth';
-import {camelCaseToDash, stringToI18nMessageKey} from '../utils'
-import {StringFormat} from '../Column/String/StringColumn';
-import {RouteLinks} from '../components/RouteAction/RouteLinks';
-import {useProperty} from '../hooks/UseProperty';
-import {useItemQuery} from '../hooks/UseItemQuery';
-import {useUri} from '../hooks/UseUri';
-import {ColumnTypeEnum} from '../types/types';
-import {ModelEnum} from '../../app/modules/types';
-import {SVG} from '../components/SVG/SVG';
-import {IconButton} from '../components/Button/IconButton';
-import {Help} from '../components/Help';
-import {ModelCellSkeleton} from '../ListingView/views/Table/ModelCell';
-import {isClinicColumn} from '../ListingView/ListingView.utils';
-import {useCurrentOperation} from '../../_metronic/layout/components/header/page-title/DefaultTitle';
-import {Button} from '../components/Button';
+import React, {Fragment} from 'react'
+import {useMapping} from '../hooks/UseMapping'
+import {ItemView} from '../components/ItemView'
+import {Skeleton} from '@mui/material'
+import {Trans} from '../components/Trans'
+import clsx from 'clsx'
+import {ColumnDef, ColumnMapping, DetailColumns, DetailViewType, Model, ViewEnum} from '../types/ModelMapping'
+import {TitleContent} from '../ListingView/views/Table/HeaderCell'
+import {ListingView} from '../ListingView/ListingView'
+import {Link} from 'react-router-dom'
+import {DetailViewColumnContent} from './DetailViewColumnContent'
+import {useAuth} from '../hooks/UseAuth'
+import {camelCaseToDash} from '../utils'
+import {StringFormat} from '../Column/String/StringColumn'
+import {useProperty} from '../hooks/UseProperty'
+import {useItemQuery} from '../hooks/UseItemQuery'
+import {useUri} from '../hooks/UseUri'
+import {ColumnTypeEnum} from '../types/types'
+import {ModelEnum} from '../../app/modules/types'
+import {isClinicColumn} from '../ListingView/ListingView.utils'
+import {ItemOverview} from './ItemOverview'
 
 export const DEFAULT_DETAIL_VIEW: DetailViewType<any> = {
   type: ViewEnum.Detail
-};
-
-export const ItemOverview = <M extends ModelEnum>({modelName, children}: {modelName: M} & HTMLAttributes<HTMLDivElement>) => {
-  const {views} = useMapping<M>({modelName});
-  const { id:uid } = useParams<{ id: string }>();
-  const {operations} = useAuth();
-  const currentOperation = useCurrentOperation();
-  const {item, isLoading} = useItemQuery<M>({
-    modelName,
-    // enabled: isOverview || (typeof column !== 'boolean' && column?.as === 'TAB')
-  });
-
-  const view = (views?.find(view => view.type === ViewEnum.Detail) || DEFAULT_DETAIL_VIEW) as DetailViewType<M>;
-  const {itemOperationRoutes, customActions} = view;
-  const _operations = useMemo(() => {
-    if (!item) {
-      return [];
-    }
-
-    const itemOperations = operations.filter(({ resource, operationType }) => {
-      if (resource.name !== modelName) return false
-
-      return currentOperation?.operationType &&
-        [ViewEnum.Listing, ViewEnum.Update, ViewEnum.Delete, ViewEnum.Detail]
-          .filter(viewEnum => viewEnum !== currentOperation.operationType)
-          .includes(operationType)
-    });
-
-    return itemOperationRoutes?.({ item, operations: itemOperations }) || itemOperations;
-  }, [item]);
-
-  return (
-    <div className='card mb-3'>
-      <div className={clsx('card-body', children && 'pb-0')}>
-        <div className={clsx('d-flex flex-wrap flex-xs-nowrap', children && 'mb-3')}>
-          {isLoading && (
-            <ModelCellSkeleton
-              iconSize={75}
-              titleHeight={26}
-              subTitleHeight={25}
-            />
-          )}
-          {item?.['@icon'] && (
-            <div className='symbol symbol-50px me-3'>
-              <div className='symbol-label bg-light-primary'>
-                <SVG path={item['@icon']} size='3x' variant='primary'/>
-              </div>
-            </div>
-          )}
-          <div className='flex-grow-1'>
-            <div className='d-flex justify-content-between align-items-start flex-wrap gap-3'>
-              <div className="d-flex flex-column">
-                <div className="d-flex flex-wrap fw-semibold fs-5 text-gray-500 ">
-                  {item && <Trans id={stringToI18nMessageKey(item?.['@type'])} />}
-                </div>
-                <div className="d-flex align-items-center">
-                  <a href="#" className="text-gray-800 text-hover-primary fs-2 fw-bold mt-1 me-3">
-                    {item?.['@title']}
-                  </a>
-                </div>
-                {/*<Help overlay='Copier' placement='right' className='mt-1 d-flex'>*/}
-                {/*  <a*/}
-                {/*    href='#'*/}
-                {/*    className='d-flex align-items-center bg-gray-200 rounded ps-2 text-muted text-hover-primary'*/}
-                {/*    onClick={e => {*/}
-                {/*      e.preventDefault();*/}
-                {/*      if (uid) {*/}
-                {/*        navigator.clipboard.writeText(uid);*/}
-                {/*      }*/}
-                {/*    }}*/}
-                {/*  >*/}
-                {/*    <small className='fw-bold fs-8'>*/}
-                {/*      {uid}*/}
-                {/*    </small>*/}
-                {/*    <IconButton path='/general/gen054.svg' size='2'/>*/}
-                {/*  </a>*/}
-                {/*</Help>*/}
-              </div>
-              <div className="d-flex gap-3">
-                {item && customActions?.map(({render}, index) => (
-                  <Fragment key={index}>
-                    {render({item})}
-                  </Fragment>
-                ))}
-                {uid && (
-                  <RouteLinks
-                    operations={_operations}
-                    params={{id: uid}}
-                    // useContextualTitle
-                  />
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-        {children}
-      </div>
-    </div>
-  );
 };
 
 export const DetailView = <M extends ModelEnum>({modelName}: { modelName: M }) => {
