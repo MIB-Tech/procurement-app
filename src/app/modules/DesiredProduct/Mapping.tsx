4ba90c20787c9300a17bd1b239023c5ea6d39d86
ozil48 <amara.mohammed@outlook.com>
2024-06-06 13:25:00 +0100

    add max lenght designation

--- src/app/modules/DesiredProduct/Mapping.tsx
+++ src/app/modules/DesiredProduct/Mapping.tsx
@@ -54,6 +54,7 @@ const mapping: ModelMapping<ModelEnum.DesiredProduct> = {
     },
     designation: {
       type: ColumnTypeEnum.String,
+      max: 1000,
     },
     quantity: {
       type: ColumnTypeEnum.Number,
